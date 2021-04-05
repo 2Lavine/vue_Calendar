4a0694f45a5b85c3a14605d83218ff6e7843dd27
katemic <bigideahuang@foxmail.com>
2021-04-05 21:06:58 +0800

    update calendarData(week days ...)

--- src/components/MiniCalendar.vue
+++ src/components/MiniCalendar.vue
@@ -1,13 +1,24 @@
 <template lang="">
     <div>
-        
+        {{calendarData.year}}年{{calendarData.month+1}}月
     </div>
 </template>
 <script>
 import calendarData from "@/data/calendarData";
 console.log(calendarData);
+let { year, month } = calendarData;
 export default {
   name: "MiniCalendar",
+  data() {
+    return {
+      calendarData: {
+        year,
+        month,
+      },
+    };
+  },
+  created () {
+  }
 };
 </script>
 <style>
--- src/data/calendarData.js
+++ src/data/calendarData.js
@@ -1,4 +1,37 @@
-const calendatData =Object.create(null);
-const DAYS = [0,31,28,31,30,31,30,31,31,30,31,30,31];
-calendatData.DAYS = DAYS;
-export default calendatData;
+const MONTHDAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const SUMMONTHDAYS = MONTHDAYS.reduce(
+  (res, cur, index) => {
+    console.log(res, cur, index, res[index]);
+    res.push(cur + res[index]);
+    return res;
+  },
+  [0]
+)
+const date = new Date();
+let MONTH = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"];
+let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+let weekDay = date.getDay(); // start on Sunday (0=>Sunday)
+let FIRSTWEEKDAY = new Date(year, month, 0).getDay();
+let LASTTWEEKDAY = new Date(year, month, MONTHDAYS[month]).getDay();
+let preDaysStart =
+  month !== 0
+    ? MONTHDAYS[month - 1] - FIRSTWEEKDAY - 1
+    : MONTHDAYS[11] - FIRSTWEEKDAY - 1;
+let remainDaysCount = 6 - LASTTWEEKDAY;
+
+let calendarData = {
+  MONTH,
+  day,
+  month,
+  year,
+  weekDay,
+  FIRSTWEEKDAY,
+  LASTTWEEKDAY,
+  SUMMONTHDAYS,
+  preDaysStart,
+  remainDaysCount,
+  MONTHDAYS,
+};
+export default calendarData;
