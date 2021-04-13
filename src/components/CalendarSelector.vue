2b0e1c70a5cf3ce34ed1d3a1646ddc231c487aa2
katemic <bigideahuang@foxmail.com>
2021-04-13 19:38:50 +0800

    add Calendarselector

---
+++ src/components/CalendarSelector.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="CalendarSelector">
+    <div v-for="mode in modeArray" :key="mode" :class="modeItem">
+      {{ mode }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      modeArray: ["日", "周", "月", "年"],
+    };
+  },
+};
+</script>
+<style></style>
